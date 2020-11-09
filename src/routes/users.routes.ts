53036cb33d03d764f51c34dab008fa47b6799575
ErickCReis <erickcorreareis@gmail.com>
2020-11-09 12:23:47 -0300

    :sparkles: saving avatar in database

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import multer from 'multer';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middleware/ensureAuthenticated';
 import uploadConfig from '../config/upload';
@@ -16,9 +18,16 @@ usersRouter.post('/', async (request, response) => {
 
     const user = await createUser.execute({ name, email, password });
 
-    delete user.password;
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
 
-    return response.status(201).json(user);
+    return response.status(201).json(userWithoutPassword);
   } catch (err) {
     return response.status(400).json({ message: err.message });
   }
@@ -29,7 +38,27 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({ ok: true });
+    try {
+      const uploadUserAvatar = new UpdateUserAvatarService();
+
+      const user = await uploadUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+
+      return response.json(userWithoutPassword);
+    } catch (err) {
+      return response.status(400).json({ message: err.message });
+    }
   },
 );
 
