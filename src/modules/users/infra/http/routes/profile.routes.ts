577782dfaa191ae8d3d2e4fd310c2cacc4de34fd
ErickCReis <erickcorreareis@gmail.com>
2021-01-16 13:15:23 -0300

    :sparkles: validating data

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
-import ProfileController from '../controllers/ProfileController';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '../middleware/ensureAuthenticated';
+
+import ProfileController from '../controllers/ProfileController';
 
 const profileRouter = Router();
 const profileController = new ProfileController();
@@ -9,6 +11,24 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.when('old_password', {
+        is: Joi.exist(),
+        then: Joi.required(),
+      }),
+      password_confirmation: Joi.when('password', {
+        is: Joi.exist(),
+        then: Joi.valid(Joi.ref('password')).required(),
+      }),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
