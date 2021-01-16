0290e5be447da273370e2c1a5405eb1887e4afc1
ErickCReis <erickcorreareis@gmail.com>
2021-01-16 10:37:13 -0300

    :sparkles: adding user id in appointments

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -17,6 +17,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '123',
       provider_id: '123',
     });
 
@@ -30,12 +31,14 @@ describe('CreateAppointment', () => {
     await createAppointment.execute({
       date: apointmentDate,
       provider_id: '123',
+      user_id: '123',
     });
 
     await expect(
       createAppointment.execute({
         date: apointmentDate,
         provider_id: '123',
+        user_id: '123',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
